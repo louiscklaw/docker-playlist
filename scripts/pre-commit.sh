e9b3398704389424c024d8e8081a9fa1486d9a32
louiscklaw <louiscklaw@gmail.com>
2020-09-08 23:20:53 +0800

    update,

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -6,4 +6,4 @@ echo 'updating master_build.yml...'
 ./update_main_build_chain.sh
 
 echo 'adding master_build.yml into commit...'
-git add .
+git add /home/logic/_workspace/docker-playlist/.github/workflows/master_build.yml
