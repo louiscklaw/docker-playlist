b71aa9c13a042002f95ca45e59d5c3bcd8e9ffde
louiscklaw <louiscklaw@gmail.com>
2022-04-06 22:37:41 +0800

    update,

---
+++ docker-strapi-tryout/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e5db89ebed7f0c0f326dcfffbb60140c'),
+    },
+  },
+});
