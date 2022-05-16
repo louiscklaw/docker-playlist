dd949ca14aa1a2648c9644d6940496a28d991ab2
Louis Law <louiscklaw@users.noreply.github.com>
2022-05-16 14:46:08 +0800

    update docker-react-tryout,

--- docker-react-tryout/admin_cms/src/App.js
+++ docker-react-tryout/admin_cms/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello restaurant cms</div>;
+  return <div className="App">hello admin cms</div>;
 }
 
 export default App;
