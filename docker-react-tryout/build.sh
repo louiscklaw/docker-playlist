3edd7a6d0f538b5a4aa58160e58f747eaa8dbd60
louiscklaw <louiscklaw@gmail.com>
2022-07-07 16:14:18 +0800

    update docker-react-tryout,

--- docker-react-tryout/build.sh
+++ docker-react-tryout/build.sh
@@ -5,3 +5,6 @@ set -ex
 
 docker build -t logickee/wiki-app-tryout wiki
 docker image push logickee/wiki-app-tryout
+
+docker build -t logickee/restaurant-cms-tryout restaurant_cms
+docker image push logickee/restaurant-cms-tryout
