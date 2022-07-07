f5a0482aa21d0b1377e82abfd9f40895487e6705
louiscklaw <louiscklaw@gmail.com>
2022-07-08 02:44:40 +0800

    update docker-react-tryout,

--- docker-react-tryout/build.sh
+++ docker-react-tryout/build.sh
@@ -3,6 +3,9 @@
 set -ex
 
 
+docker build -t logickee/nextjs-helloworld-tryout nextjs_helloworld_js
+docker image push logickee/nextjs-helloworld-tryout
+
 docker build -t logickee/wiki-app-tryout wiki
 docker image push logickee/wiki-app-tryout
 
