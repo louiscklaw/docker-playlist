64db61cf2d7ab571174ac3893cbb448c823ced2c
louiscklaw <louiscklaw@gmail.com>
2022-07-26 12:18:17 +0800

    update docker-mailhog-tryout,

--- docker-mailhog-tryout/test/index.js
+++ docker-mailhog-tryout/test/index.js
@@ -1,14 +1,14 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
 
 const transport = nodemailer.createTransport({
-  host: "localhost",
-  port: "1025",
+  host: 'localhost',
+  port: '1025',
   auth: null,
-});
+})
 
 transport.sendMail({
-  from: "test_from <test_from@gmail.com",
-  to: "test_to <test_to@gmail.com",
-  subject: "test subject",
-  html: "<h1>helloworld</h1>",
-});
+  from: 'test_from <test_from@gmail.com',
+  to: 'test_to <test_to@gmail.com',
+  subject: 'test subject',
+  html: '<h1>helloworld</h1>',
+})
