238807271a6a08fc268e626346dd57addfd16823
louiscklaw <logickee@gmail.com>
2022-11-10 02:13:34 +0800

    update,

--- docker-changedetection-tryout/start_docker_compose.sh
+++ docker-changedetection-tryout/start_docker_compose.sh
@@ -7,14 +7,14 @@ docker compose -f ./docker-compose-private-changedetect.yml kill
 docker compose -f ./docker-compose-private-changedetect.yml down
 docker compose -f ./docker-compose-private-changedetect.yml up -d --build
 
-docker compose -f ./docker-compose-changedetection-io.yml kill
-docker compose -f ./docker-compose-changedetection-io.yml down
-docker compose -f ./docker-compose-changedetection-io.yml up -d --build
+# docker compose -f ./docker-compose-changedetection-io.yml kill
+# docker compose -f ./docker-compose-changedetection-io.yml down
+# docker compose -f ./docker-compose-changedetection-io.yml up -d --build
 
-docker compose -f ./docker-compose-taobao-changedetect.yml kill
-docker compose -f ./docker-compose-taobao-changedetect.yml down
-docker compose -f ./docker-compose-taobao-changedetect.yml up -d --build
+# docker compose -f ./docker-compose-taobao-changedetect.yml kill
+# docker compose -f ./docker-compose-taobao-changedetect.yml down
+# docker compose -f ./docker-compose-taobao-changedetect.yml up -d --build
 
-docker compose -f ./docker-compose-carousell-changedetect.yml kill
-docker compose -f ./docker-compose-carousell-changedetect.yml down
-docker compose -f ./docker-compose-carousell-changedetect.yml up -d --build
+# docker compose -f ./docker-compose-carousell-changedetect.yml kill
+# docker compose -f ./docker-compose-carousell-changedetect.yml down
+# docker compose -f ./docker-compose-carousell-changedetect.yml up -d --build
