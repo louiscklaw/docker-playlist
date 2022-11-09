94f908b4cc99582af6d55f378ca019e75397fe42
louiscklaw <logickee@gmail.com>
2022-11-10 01:53:50 +0800

    update,

--- docker-changedetection-tryout/start_docker_compose.sh
+++ docker-changedetection-tryout/start_docker_compose.sh
@@ -5,9 +5,9 @@ set -ex
 
 docker compose -f ./docker-compose-private-changedetect.yml kill
 docker compose -f ./docker-compose-private-changedetect.yml down
-docker compose -f ./docker-compose-private-changedetect.yml up -d --build
+docker compose -f ./docker-compose-private-changedetect.yml up -d --build --remove-orphans
 
-docker compose -f ./docker-compose-changedetection-io.yml up -d --build
-docker compose -f ./docker-compose-taobao-changedetect.yml up -d --build
+docker compose -f ./docker-compose-changedetection-io.yml up -d --build --remove-orphans
+docker compose -f ./docker-compose-taobao-changedetect.yml up -d --build --remove-orphans
 
-docker compose -f ./docker-compose-carousell-changedetect.yml up -d --build
+docker compose -f ./docker-compose-carousell-changedetect.yml up -d --build --remove-orphans
