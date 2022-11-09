039b579250c80bf65117e462035c3ec31c3bc5ff
louiscklaw <logickee@gmail.com>
2022-11-10 01:06:41 +0800

    update,

--- docker-changedetection-tryout/start_docker_compose.sh
+++ docker-changedetection-tryout/start_docker_compose.sh
@@ -3,7 +3,7 @@
 set -ex
 
 
-docker compose -f ./docker-compose-changedetection-io.yml up --build
-docker compose -f ./docker-compose-taobao-changedetect.yml up --build
-docker compose -f ./docker-compose-private-changedetect.yml up --build
-docker compose -f ./docker-compose-carousell-changedetect.yml up --build
+docker compose -f ./docker-compose-changedetection-io.yml up -d --build
+docker compose -f ./docker-compose-taobao-changedetect.yml up -d --build
+docker compose -f ./docker-compose-private-changedetect.yml up -d --build
+docker compose -f ./docker-compose-carousell-changedetect.yml up -d --build
