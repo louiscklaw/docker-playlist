88b69541c9a73a9a126acbc0ffc9f3e60f6f6234
louiscklaw <louiscklaw@user.noreply.github.com>
2022-12-11 23:25:08 +0800

    update docker-rotating-proxy-tryout,

---
+++ docker-rotating-proxy-tryout/start_docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# sudo rm -rf data
+
+docker-compose build
+docker-compose kill
+docker-compose up -d
+docker-compose logs -f
---
+++ docker-rotating-proxy-tryout/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -ex
+
+
+
+# curl --proxy 127.0.0.1:5566 https://api.my-ip.io/ip
+
+for ((i = 0 ; i < 100 ; i++)); do
+  curl --proxy 192.168.10.61:5566 https://api.my-ip.io/ip
+done
