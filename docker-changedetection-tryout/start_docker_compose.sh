f65d22f5c1d917298b87198a1ec2b2aed4f5ea79
louiscklaw <louiscklaw@user.noreply.github.com>
2023-02-08 15:33:14 +0800

    update,

--- docker-changedetection-tryout/start_docker_compose.sh
+++ docker-changedetection-tryout/start_docker_compose.sh
@@ -2,28 +2,32 @@
 
 set -ex
 
-
 docker compose -f ./docker-compose-playwright.yml kill
 docker compose -f ./docker-compose-playwright.yml down
 docker compose -f ./docker-compose-playwright.yml up -d --build
+# docker compose -f ./docker-compose-playwright.yml logs -f
 
-docker compose -f ./docker-compose-new-changedetect.yml kill
-docker compose -f ./docker-compose-new-changedetect.yml down
-docker compose -f ./docker-compose-new-changedetect.yml up -d --build
-
-docker compose -f ./docker-compose-taobao-changedetect.yml kill
-docker compose -f ./docker-compose-taobao-changedetect.yml down
-docker compose -f ./docker-compose-taobao-changedetect.yml up -d --build
+docker compose -f ./docker-compose-helloworld.yml kill
+docker compose -f ./docker-compose-helloworld.yml down
+docker compose -f ./docker-compose-helloworld.yml up -d --build
 
 docker compose -f ./docker-compose-carousell-changedetect.yml kill
 docker compose -f ./docker-compose-carousell-changedetect.yml down
 docker compose -f ./docker-compose-carousell-changedetect.yml up -d --build
 
-# docker compose -f ./docker-compose-changedetection-io.yml kill
-# docker compose -f ./docker-compose-changedetection-io.yml down
-# docker compose -f ./docker-compose-changedetection-io.yml up -d --build
+docker compose -f ./docker-compose-private-changedetect.yml kill
+docker compose -f ./docker-compose-private-changedetect.yml down
+docker compose -f ./docker-compose-private-changedetect.yml up -d --build
 
-#docker compose -f ./docker-compose-playwright.yml kill
+docker compose -f ./docker-compose-taobao-changedetect.yml kill
+docker compose -f ./docker-compose-taobao-changedetect.yml down
+docker compose -f ./docker-compose-taobao-changedetect.yml up -d --build
+
+# docker compose -f ./docker-compose-changedetection-io.yml kill
+# # docker compose -f ./docker-compose-changedetection-io.yml down
+# # docker compose -f ./docker-compose-changedetection-io.yml up -d --build
+
+# docker compose -f ./docker-compose-playwright.yml kill
 
 #sleep 1
 
