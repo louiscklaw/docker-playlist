5de9a50c7576410bc381ff430a91a52767f2dfef
louiscklaw <louiscklaw@user.noreply.github.com>
2023-02-10 21:53:37 +0800

    update docker-changedetection-tryout,

--- docker-changedetection-tryout/start_docker_compose.sh
+++ docker-changedetection-tryout/start_docker_compose.sh
@@ -2,26 +2,39 @@
 
 set -ex
 
-docker compose -f ./docker-compose-playwright.yml kill
-docker compose -f ./docker-compose-playwright.yml down
-docker compose -f ./docker-compose-playwright.yml up -d --build
-# docker compose -f ./docker-compose-playwright.yml logs -f
+pushd avgle
+  docker compose kill
+  docker compose down
+  docker compose up -d --build
+  docker compose logs -f
+popd
 
-docker compose -f ./docker-compose-helloworld.yml kill
-docker compose -f ./docker-compose-helloworld.yml down
-docker compose -f ./docker-compose-helloworld.yml up -d --build
 
-docker compose -f ./docker-compose-carousell-changedetect.yml kill
-docker compose -f ./docker-compose-carousell-changedetect.yml down
-docker compose -f ./docker-compose-carousell-changedetect.yml up -d --build
+# docker compose -f ./docker-compose-debug-browserless-chrome.yml kill
+# docker compose -f ./docker-compose-debug-browserless-chrome.yml down
+# docker compose -f ./docker-compose-debug-browserless-chrome.yml up -d --build
+# docker compose -f ./docker-compose-debug-browserless-chrome.yml logs -f
 
-docker compose -f ./docker-compose-private-changedetect.yml kill
-docker compose -f ./docker-compose-private-changedetect.yml down
-docker compose -f ./docker-compose-private-changedetect.yml up -d --build
+# docker compose -f ./docker-compose-playwright.yml kill
+# docker compose -f ./docker-compose-playwright.yml down
+# docker compose -f ./docker-compose-playwright.yml up -d --build
+# # docker compose -f ./docker-compose-playwright.yml logs -f
 
-docker compose -f ./docker-compose-taobao-changedetect.yml kill
-docker compose -f ./docker-compose-taobao-changedetect.yml down
-docker compose -f ./docker-compose-taobao-changedetect.yml up -d --build
+# docker compose -f ./docker-compose-helloworld.yml kill
+# docker compose -f ./docker-compose-helloworld.yml down
+# docker compose -f ./docker-compose-helloworld.yml up -d --build
+
+# docker compose -f ./docker-compose-carousell-changedetect.yml kill
+# docker compose -f ./docker-compose-carousell-changedetect.yml down
+# docker compose -f ./docker-compose-carousell-changedetect.yml up -d --build
+
+# docker compose -f ./docker-compose-private-changedetect.yml kill
+# docker compose -f ./docker-compose-private-changedetect.yml down
+# docker compose -f ./docker-compose-private-changedetect.yml up -d --build
+
+# docker compose -f ./docker-compose-taobao-changedetect.yml kill
+# docker compose -f ./docker-compose-taobao-changedetect.yml down
+# docker compose -f ./docker-compose-taobao-changedetect.yml up -d --build
 
 # docker compose -f ./docker-compose-changedetection-io.yml kill
 # # docker compose -f ./docker-compose-changedetection-io.yml down
