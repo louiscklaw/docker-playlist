46b06d54dd9b05eae545b141ebff0ddfb2590a44
louiscklaw <louiscklaw@user.noreply.github.com>
2023-02-12 13:46:08 +0800

    update,

--- docker-changedetection-tryout/start_docker_compose.sh
+++ docker-changedetection-tryout/start_docker_compose.sh
@@ -2,40 +2,54 @@
 
 set -ex
 
-pushd avgle
+pushd taobao
+  docker compose kill
+  docker compose down
+  docker compose up -d --build &
+  # docker compose logs -f
+# popd
+
+pushd carousell
   docker compose kill
   docker compose down
   docker compose up -d --build &
   # docker compose logs -f
 popd
 
-pushd av01.tv
-  docker compose kill
-  docker compose down
-  docker compose up -d --build &
-  # docker compose logs -f
-popd
+# pushd avgle
+#   docker compose kill
+#   docker compose down
+#   docker compose up -d --build &
+#   # docker compose logs -f
+# popd
 
-pushd jable.tv
-  docker compose kill
-  docker compose down
-  docker compose up -d --build &
-  # docker compose logs -f
-popd
+# pushd av01.tv
+#   docker compose kill
+#   docker compose down
+#   docker compose up -d --build &
+#   # docker compose logs -f
+# popd
 
-pushd kissjav
-  docker compose kill
-  docker compose down
-  docker compose up -d --build &
-  # docker compose logs -f
-popd
+# pushd jable.tv
+#   docker compose kill
+#   docker compose down
+#   docker compose up -d --build &
+#   # docker compose logs -f
+# popd
 
-pushd missav
-  docker compose kill
-  docker compose down
-  docker compose up -d --build &
-  # docker compose logs -f
-popd
+# pushd kissjav
+#   docker compose kill
+#   docker compose down
+#   docker compose up -d --build &
+#   # docker compose logs -f
+# popd
+
+# pushd missav
+#   docker compose kill
+#   docker compose down
+#   docker compose up -d --build &
+#   # docker compose logs -f
+# popd
 
 
 wait 
