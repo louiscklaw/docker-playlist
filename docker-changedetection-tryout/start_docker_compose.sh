c1325f81c8d8c980fff6dca4f3bcaf4464cd8c80
louiscklaw <louiscklaw@user.noreply.github.com>
2023-03-04 23:24:45 +0800

    update,

--- docker-changedetection-tryout/start_docker_compose.sh
+++ docker-changedetection-tryout/start_docker_compose.sh
@@ -2,54 +2,40 @@
 
 set -ex
 
-# pushd taobao
-#   docker compose kill
-#   docker compose down
-#   docker compose up -d --build &
-#   # docker compose logs -f
-# # popd
-
-# pushd carousell
-#   docker compose kill
-#   docker compose down
-#   docker compose up -d --build &
-#   # docker compose logs -f
-# popd
-
-# pushd avgle
-#   docker compose kill
-#   docker compose down
-#   docker compose up -d --build &
-#   # docker compose logs -f
-# popd
-
-# pushd av01.tv
-#   docker compose kill
-#   docker compose down
-#   docker compose up -d --build &
-#   # docker compose logs -f
-# popd
-
-pushd jable.tv
+pushd debug
   docker compose kill
   docker compose down
-  docker compose up -d --build &
+  docker compose up -d --build 
+#   # docker compose logs -f
+popd
+
+pushd carousell
+  docker compose kill
+  docker compose down
+  docker compose up -d --build 
   # docker compose logs -f
 popd
 
-# pushd kissjav
-#   docker compose kill
-#   docker compose down
-#   docker compose up -d --build &
+pushd taobao
+  docker compose kill
+  docker compose down 
+  docker compose up -d --build 
 #   # docker compose logs -f
-# popd
+popd
 
-# pushd missav
-#   docker compose kill
-#   docker compose down
-#   docker compose up -d --build &
+pushd changedetect
+  docker compose kill
+  docker compose down
+  docker compose up -d --build 
+  # docker compose logs -f
+popd
+
+pushd private
+  docker compose kill
+  docker compose down
+  docker compose up -d --build 
 #   # docker compose logs -f
-# popd
+popd
 
 
 wait 
