ec81e0a003785887f1fe85c3eb06ff266807458a
louiscklaw <louiscklaw@users.noreply.github.com>
2023-04-30 02:55:37 +0800

    update docker-healthcheck-tryout

--- docker-healthcheck-tryout/start_docker.sh
+++ docker-healthcheck-tryout/start_docker.sh
@@ -4,7 +4,7 @@ set -ex
 
 # sudo rm -rf data
 
-docker-compose build
-docker-compose kill
-docker-compose up -d
-docker-compose logs -f
+docker compose build
+docker compose kill
+docker compose up -d
+docker compose logs -f
