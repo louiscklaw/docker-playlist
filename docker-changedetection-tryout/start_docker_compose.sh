9b6608d3b43dc4b92a740d7dc68c4b505b2b034b
louiscklaw <louiscklaw@users.noreply.github.com>
2023-04-30 03:13:34 +0800

    update docker-playlist,

--- docker-changedetection-tryout/start_docker_compose.sh
+++ docker-changedetection-tryout/start_docker_compose.sh
@@ -2,33 +2,52 @@
 
 set -ex
 
-# pushd taobao
-#   docker compose kill
-#   docker compose down
-#   docker compose up -d --build &
-#   # docker compose logs -f
-# # popd
+docker compose -f ./docker-compose-playwright.yml kill
+docker compose -f ./docker-compose-playwright.yml down
+docker compose -f ./docker-compose-playwright.yml up -d --build
+# docker compose -f ./docker-compose-playwright.yml logs -f
 
-# pushd carousell
-#   docker compose kill
-#   docker compose down
-#   docker compose up -d --build &
-#   # docker compose logs -f
-# popd
+docker compose -f ./docker-compose-changedetection-io.yml kill
+docker compose -f ./docker-compose-changedetection-io.yml down
+docker compose -f ./docker-compose-changedetection-io.yml up -d --build
 
-# pushd avgle
-#   docker compose kill
-#   docker compose down
-#   docker compose up -d --build &
-#   # docker compose logs -f
-# popd
+# docker compose -f ./docker-compose-changedetection-io.yml logs -f
 
-# pushd av01.tv
-#   docker compose kill
-#   docker compose down
-#   docker compose up -d --build &
-#   # docker compose logs -f
-# popd
+
+pushd carousell
+  docker compose kill
+  docker compose down
+  docker compose up -d --build &
+  # docker compose logs -f
+popd
+
+pushd taobao
+  docker compose kill
+  docker compose down
+  docker compose up -d --build &
+  # docker compose logs -f
+popd
+
+pushd missav
+  docker compose kill
+  docker compose down
+  docker compose up -d --build &
+  # docker compose logs -f
+popd
+
+pushd avgle
+  docker compose kill
+  docker compose down
+  docker compose up -d --build &
+  # docker compose logs -f
+popd
+
+pushd av01.tv
+  docker compose kill
+  docker compose down
+  docker compose up -d --build &
+  # docker compose logs -f
+popd
 
 pushd jable.tv
   docker compose kill
@@ -37,59 +56,13 @@ pushd jable.tv
   # docker compose logs -f
 popd
 
-# pushd kissjav
-#   docker compose kill
-#   docker compose down
-#   docker compose up -d --build &
-#   # docker compose logs -f
-# popd
+pushd kissjav
+  docker compose kill
+  docker compose down
+  docker compose up -d --build &
+  # docker compose logs -f
+popd
 
-# pushd missav
-#   docker compose kill
-#   docker compose down
-#   docker compose up -d --build &
-#   # docker compose logs -f
-# popd
+sudo chmod 777 -R ./volumes
 
-
-wait 
-
-# docker compose -f ./docker-compose-debug-browserless-chrome.yml kill
-# docker compose -f ./docker-compose-debug-browserless-chrome.yml down
-# docker compose -f ./docker-compose-debug-browserless-chrome.yml up -d --build
-# docker compose -f ./docker-compose-debug-browserless-chrome.yml logs -f
-
-# docker compose -f ./docker-compose-playwright.yml kill
-# docker compose -f ./docker-compose-playwright.yml down
-# docker compose -f ./docker-compose-playwright.yml up -d --build
-# # docker compose -f ./docker-compose-playwright.yml logs -f
-
-# docker compose -f ./docker-compose-helloworld.yml kill
-# docker compose -f ./docker-compose-helloworld.yml down
-# docker compose -f ./docker-compose-helloworld.yml up -d --build
-
-# docker compose -f ./docker-compose-carousell-changedetect.yml kill
-# docker compose -f ./docker-compose-carousell-changedetect.yml down
-# docker compose -f ./docker-compose-carousell-changedetect.yml up -d --build
-
-# docker compose -f ./docker-compose-private-changedetect.yml kill
-# docker compose -f ./docker-compose-private-changedetect.yml down
-# docker compose -f ./docker-compose-private-changedetect.yml up -d --build
-
-# docker compose -f ./docker-compose-taobao-changedetect.yml kill
-# docker compose -f ./docker-compose-taobao-changedetect.yml down
-# docker compose -f ./docker-compose-taobao-changedetect.yml up -d --build
-
-# docker compose -f ./docker-compose-changedetection-io.yml kill
-# # docker compose -f ./docker-compose-changedetection-io.yml down
-# # docker compose -f ./docker-compose-changedetection-io.yml up -d --build
-
-# docker compose -f ./docker-compose-playwright.yml kill
-
-#sleep 1
-
-#docker compose -f ./docker-compose-playwright.yml down
-
-#sleep 1
-
-#docker compose -f ./docker-compose-playwright.yml up -d --build
+wait
