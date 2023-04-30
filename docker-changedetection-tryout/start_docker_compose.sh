1729e68d54801f56df73b54b4ab6b0910d514967
louiscklaw <louiscklaw@users.noreply.github.com>
2023-04-30 15:12:34 +0800

    update docker-changedetection-tryout,

--- docker-changedetection-tryout/start_docker_compose.sh
+++ docker-changedetection-tryout/start_docker_compose.sh
@@ -10,9 +10,12 @@ docker compose -f ./docker-compose-playwright.yml up -d --build
 docker compose -f ./docker-compose-changedetection-io.yml kill
 docker compose -f ./docker-compose-changedetection-io.yml down
 docker compose -f ./docker-compose-changedetection-io.yml up -d --build
-
 # docker compose -f ./docker-compose-changedetection-io.yml logs -f
 
+docker compose -f ./docker-compose-private-changedetect.yml kill
+docker compose -f ./docker-compose-private-changedetect.yml down
+docker compose -f ./docker-compose-private-changedetect.yml up -d --build
+# docker compose -f ./docker-compose-private-changedetect.yml logs -f
 
 pushd carousell
   docker compose kill
@@ -22,41 +25,6 @@ pushd carousell
 popd
 
 pushd taobao
-  docker compose kill
-  docker compose down
-  docker compose up -d --build &
-  # docker compose logs -f
-popd
-
-pushd missav
-  docker compose kill
-  docker compose down
-  docker compose up -d --build &
-  # docker compose logs -f
-popd
-
-pushd avgle
-  docker compose kill
-  docker compose down
-  docker compose up -d --build &
-  # docker compose logs -f
-popd
-
-pushd av01.tv
-  docker compose kill
-  docker compose down
-  docker compose up -d --build &
-  # docker compose logs -f
-popd
-
-pushd jable.tv
-  docker compose kill
-  docker compose down
-  docker compose up -d --build &
-  # docker compose logs -f
-popd
-
-pushd kissjav
   docker compose kill
   docker compose down
   docker compose up -d --build &
