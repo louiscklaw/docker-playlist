0f4727e7d39f743f7f1956d42f950adaff729d93
louiscklaw <louiscklaw@users.noreply.github.com>
2023-04-30 15:16:43 +0800

    update docker-changedetection-tryout,

--- docker-changedetection-tryout/start_docker_compose.sh
+++ docker-changedetection-tryout/start_docker_compose.sh
@@ -2,20 +2,27 @@
 
 set -ex
 
-docker compose -f ./docker-compose-playwright.yml kill
-docker compose -f ./docker-compose-playwright.yml down
-docker compose -f ./docker-compose-playwright.yml up -d --build
-# docker compose -f ./docker-compose-playwright.yml logs -f
+# docker compose -f ./docker-compose-playwright.yml kill
+# docker compose -f ./docker-compose-playwright.yml down
+# docker compose -f ./docker-compose-playwright.yml up -d --build
+# # docker compose -f ./docker-compose-playwright.yml logs -f
 
-docker compose -f ./docker-compose-changedetection-io.yml kill
-docker compose -f ./docker-compose-changedetection-io.yml down
-docker compose -f ./docker-compose-changedetection-io.yml up -d --build
-# docker compose -f ./docker-compose-changedetection-io.yml logs -f
+# docker compose -f ./docker-compose-changedetection-io.yml kill
+# docker compose -f ./docker-compose-changedetection-io.yml down
+# docker compose -f ./docker-compose-changedetection-io.yml up -d --build
+# # docker compose -f ./docker-compose-changedetection-io.yml logs -f
 
 docker compose -f ./docker-compose-private-changedetect.yml kill
 docker compose -f ./docker-compose-private-changedetect.yml down
 docker compose -f ./docker-compose-private-changedetect.yml up -d --build
 # docker compose -f ./docker-compose-private-changedetect.yml logs -f
+
+pushd private-changedetect
+  docker compose kill
+  docker compose down
+  docker compose up -d --build &
+  # docker compose logs -f
+popd
 
 pushd carousell
   docker compose kill
