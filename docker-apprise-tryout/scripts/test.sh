b2da51a1b08c89f28770ae2d367aeeed3cd125f8
louiscklaw <louiscklaw@users.noreply.github.com>
2023-04-30 12:28:18 +0800

    update docker-apprise-tryout,

--- docker-apprise-tryout/scripts/test.sh
+++ docker-apprise-tryout/scripts/test.sh
@@ -8,7 +8,7 @@ docker pull caronc/apprise:latest
 # Start it up:
 # /config is used for a persistent store, you do not have to mount
 #         this if you don't intend to use it.
-docker run --name apprise \
-   -p 8000:8000 \
-   -v /var/lib/apprise/config:/config \
-   -d caronc/apprise:latest
+docker run --rm \
+  -p 8000:8000 \
+  -v /var/lib/apprise/config:/config \
+  -d caronc/apprise:latest
