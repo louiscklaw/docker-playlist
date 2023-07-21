3540e02f03efcb49975f0b06a32abb9dbd97b37e
Louis Law <louiscklaw@users.noreply.github.com>
2023-07-21 13:45:00 +0800

    update,

---
+++ docker-internal-http-tryout-python/app/update_readme.sh
@@ -0,0 +1,6 @@
+npm i markdown-to-html 
+# staticrypt
+
+npx github-markdown public/index.md --stylesheet md_style.css > public/index.html
+
+# npx staticrypt public/index.html -p nssheung_nssheung_nssheung -d public
