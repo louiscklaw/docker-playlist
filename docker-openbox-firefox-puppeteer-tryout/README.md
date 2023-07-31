7a5a6982c265d51d561ec9c446b2e8c55a07d7c7
Louis Law <louiscklaw@users.noreply.github.com>
2023-07-31 13:44:31 +0800

    update to relocate openbox-firefox-puppeteer-tryout,

--- docker-openbox-firefox-puppeteer-tryout/README.md
+++
@@ -1,9 +0,0 @@
-### start
-
-```bash
-# build openbox-ubuntu image
-$ ./build.sh
-
-$ docker compose up -d
-
-```
--- docker-openbox-firefox-puppeteer-tryout/openbox-firefox/src/start_firefox.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-
-# mkdir -p /share/firefox
-# cp -r /share/firefox-seed /share/firefox
-
-firefox --profile /share/firefox-user-data
