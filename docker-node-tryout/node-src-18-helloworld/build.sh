dfcd7efd994fb1bf1aa2fa35ae1acbfcfe372fae
Louis Law <louiscklaw@users.noreply.github.com>
2023-12-17 16:14:36 +0800

    update,

---
+++ docker-node-tryout/node-src-18-helloworld/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+
+docker buildx build . -t 192.168.10.61:5000/node-src-18-helloworld
+
+docker push 192.168.10.61:5000/node-src-18-helloworld
+
+docker run -it \
+    -v ./share:/share \
+    -v ./src:/src \
+    -v ./volumes:/volumes \
+    -e NODE_PATH=/src/node_modules \
+    --workdir /src \
+    -p 8080 \
+    -u 1000:1000 \
+    192.168.10.61:5000/node-src-18-helloworld bash
+
+# sudo chown 1000:1000 -R .
+# sudo rm -rf ./src/node_modules
+
+# docker compose up
---
+++ docker-node-tryout/node-src-20-helloworld/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+
+docker buildx build . -t 192.168.10.61:5000/node-src-20-helloworld
+
+docker push 192.168.10.61:5000/node-src-20-helloworld
+
+# docker run -it \
+#     -v ./share:/share \
+#     -v ./src:/src \
+#     -v ./volumes:/volumes \
+#     -e NODE_PATH=/src/node_modules \
+#     --workdir /src \
+#     -p 8080:8080 \
+#     -u 1000:1000 \
+#     192.168.10.61:5000/node-src-20-helloworld bash
+
+# sudo chown 1000:1000 -R .
+# sudo rm -rf ./src/node_modules
+
+docker compose up
